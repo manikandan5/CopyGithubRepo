62a610eae9019a1eea7b564bc185c30c4600ac13
Manikandan Murugesan <manikandan.m@live.co.uk>
2017-01-21 09:28:12 -0500

    Update copyRepo.sh

--- copyRepo.sh
+++ copyRepo.sh
@@ -3,17 +3,13 @@ if [$2 = ""]; then
     echo "Invalid arguments. Expected input : $0 <old-repo> <new-repo>"
     exit 1
 fi
-REPOA=$1
-REPOB=$2
-echo $REPOA
-echo $REPOB
-git clone $REPOA old-dir
+git clone $1 old-dir
 cd old-dir
 git remote rm origin
 git add .
 git commit
 cd ..
-git clone $REPOB new-dir
+git clone $2 new-dir
 cd new-dir
 git remote add repo-A ../old-dir
 git pull repo-A master --allow-unrelated-histories
